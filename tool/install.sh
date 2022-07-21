8b88271b76b5606cc5014b47446331638f4a9577
jld3103 <jld3103yt@gmail.com>
2022-07-21 12:42:39 +0200

    Update fvm

--- tool/install.sh
+++ tool/install.sh
@@ -5,7 +5,7 @@ cd "$(dirname "$0")/.."
 git submodule init
 git submodule update
 fvm dart pub global activate mono_repo 6.3.0
-fvm dart pub global activate fvm 2.3.1
+fvm dart pub global activate fvm 2.4.1
 fvm install
 (
   cd external/openapi-generator
