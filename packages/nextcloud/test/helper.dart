9e09d7031f80257d9265d13f9657aa7daa8335b0
jld3103 <jld3103yt@gmail.com>
2022-12-09 08:46:08 +0100

    Update nextcloud server and notifications to 25.0.2

--- packages/nextcloud/test/helper.dart
+++ packages/nextcloud/test/helper.dart
@@ -8,7 +8,7 @@ import 'package:nextcloud/nextcloud.dart';
 import 'package:process_run/cmd_run.dart';
 import 'package:test/test.dart';
 
-const String nextcloudVersion = '25.0.1';
+const String nextcloudVersion = '25.0.2';
 const String defaultUsername = 'user1';
 const String defaultPassword = 'user1';
 
