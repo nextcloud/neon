c13ceaaf2846ca839adbf9aafc562879e61030b8
jld3103 <jld3103yt@gmail.com>
2023-02-06 10:08:53 +0100

    Update mono_repo to 6.5.0

--- tool/install.sh
+++ tool/install.sh
@@ -4,7 +4,7 @@ cd "$(dirname "$0")/.."
 
 git submodule init
 git submodule update
-dart pub global activate mono_repo 6.4.2
+dart pub global activate mono_repo 6.5.0
 dart pub global activate fvm 2.4.1
 yes | fvm install
 fvm exec mono_repo pub get
