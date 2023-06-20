9ae7a28ac14ee4b5f63828192c280354cb4386f0
jld3103 <jld3103yt@gmail.com>
2023-06-20 15:30:33 +0200

    tool: Stop setting up git submodules

--- tool/setup.sh
+++ tool/setup.sh
@@ -2,8 +2,6 @@
 set -euxo pipefail
 cd "$(dirname "$0")/.."
 
-git submodule init
-git submodule update
 dart pub global activate melos 3.0.1
 dart pub global activate mono_repo 6.5.6
 dart pub global activate fvm 2.4.1
