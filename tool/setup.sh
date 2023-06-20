de3c972e773bb6e991b9fe502122d1e63c2a2c76
jld3103 <jld3103yt@gmail.com>
2023-06-20 15:21:48 +0200

    tool: Fix broken pipe when installing flutter

--- tool/setup.sh
+++ tool/setup.sh
@@ -5,5 +5,5 @@ cd "$(dirname "$0")/.."
 dart pub global activate melos 3.0.1
 dart pub global activate mono_repo 6.5.6
 dart pub global activate fvm 2.4.1
-yes | fvm install
+echo "y" | fvm install
 fvm exec mono_repo pub get
