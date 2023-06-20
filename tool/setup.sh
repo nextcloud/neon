3eaeb4586f98bdb260cadc3573eb9172392d3ef7
jld3103 <jld3103yt@gmail.com>
2023-06-20 15:27:50 +0200

    tool: Use single job for pub get to avoid failure

--- tool/setup.sh
+++ tool/setup.sh
@@ -6,4 +6,4 @@ dart pub global activate melos 3.0.1
 dart pub global activate mono_repo 6.5.6
 dart pub global activate fvm 2.4.1
 echo "y" | fvm install
-fvm exec mono_repo pub get
+melos exec --concurrency=1 flutter pub get
