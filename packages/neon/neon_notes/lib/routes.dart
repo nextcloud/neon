a0fd473ddd744ba822b51a5bd976a34ae13d3bf9
Nikolas Rimikis <rimikis.nikolas@gmail.com>
2023-06-26 18:29:03 +0200

    neon_notes: add app route

---
+++ packages/neon/neon_notes/lib/routes.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/widgets.dart';
+import 'package:go_router/go_router.dart';
+import 'package:neon/utils.dart';
+import 'package:neon_notes/neon_notes.dart';
+
+part 'routes.g.dart';
+
+@TypedGoRoute<NotesAppRoute>(
+  path: '$appsRoutePrefix${NotesApp.appId}',
+  name: NotesApp.appId,
+)
+@immutable
+class NotesAppRoute extends NeonAppRoute {
+  const NotesAppRoute();
+
+  @override
+  Widget build(final BuildContext context, final GoRouterState state) => const NotesMainPage();
+}
