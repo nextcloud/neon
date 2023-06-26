38bbbc7360e602b43a53679d2b1f34a0728ea93a
Nikolas Rimikis <rimikis.nikolas@gmail.com>
2023-06-26 17:59:04 +0200

    neon: test HexColor

--- packages/neon/neon/lib/src/utils/hex_color.dart
+++ packages/neon/neon/lib/src/utils/hex_color.dart
@@ -1,11 +1,11 @@
 part of '../../neon.dart';
 
 class HexColor extends Color {
-  HexColor(final String hexColor)
-      : super(
-          int.parse(
-            hexColor.toUpperCase().replaceAll('#', '').padLeft(8, 'F'),
-            radix: 16,
-          ),
-        );
+  HexColor(final String hexColor) : super(_parse(hexColor));
+
+  static int _parse(final String hexColor) {
+    final formated = hexColor.toUpperCase().replaceAll('#', '').padLeft(8, 'F');
+
+    return int.parse(formated, radix: 16);
+  }
 }
