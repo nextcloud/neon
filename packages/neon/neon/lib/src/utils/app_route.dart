d2893ff2391a4d31c87c859b544f7d41f40d80f3
Nikolas Rimikis <rimikis.nikolas@gmail.com>
2023-06-26 18:29:02 +0200

    neon: enable apps to specify go_routes

---
+++ packages/neon/neon/lib/src/utils/app_route.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/widgets.dart';
+import 'package:go_router/go_router.dart';
+
+/// [RouteData] for the initial page of an app.
+///
+/// Subclasses must override one of [build] or [redirect].
+abstract class NeonAppRoute extends GoRouteData {
+  const NeonAppRoute();
+
+  @override
+  Page<void> buildPage(final BuildContext context, final GoRouterState state) => NoTransitionPage(
+        child: build(context, state),
+      );
+}
+
+const appsRoutePrefix = '/apps/';
