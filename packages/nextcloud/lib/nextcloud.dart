d6a9604b534af203d930e4941b659e9af0705005
jld3103 <jld3103yt@gmail.com>
2023-08-05 10:33:35 +0200

    fix(nextcloud): Fix exported clients

--- packages/nextcloud/lib/nextcloud.dart
+++ packages/nextcloud/lib/nextcloud.dart
@@ -1,10 +1,13 @@
 export 'package:crypton/crypton.dart' show RSAKeypair, RSAPrivateKey, RSAPublicKey;
 
 export 'src/api/core.openapi.dart';
+export 'src/api/files_sharing.openapi.dart';
 export 'src/api/news.openapi.dart';
 export 'src/api/notes.openapi.dart';
 export 'src/api/notifications.openapi.dart';
 export 'src/api/provisioning_api.openapi.dart';
+export 'src/api/settings.openapi.dart';
+export 'src/api/theming.openapi.dart';
 export 'src/api/uppush.openapi.dart';
 export 'src/api/user_status.openapi.dart';
 export 'src/app_type.dart';
