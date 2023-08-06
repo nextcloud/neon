6c22b8f9e5989b77cf68e5722cec7a0194894617
jld3103 <jld3103yt@gmail.com>
2023-08-06 17:36:05 +0200

    refactor(neon): Align colors with design guidelines

--- packages/neon/neon/lib/src/theme/colors.dart
+++ packages/neon/neon/lib/src/theme/colors.dart
@@ -6,9 +6,23 @@ import 'package:nextcloud/nextcloud.dart';
 abstract final class NcColors {
   /// Nextcloud blue.
   ///
-  /// The default primary clolor as specified by the
-  /// [design guidlines](https://docs.nextcloud.com/server/latest/developer_manual/design/foundations.html#primary-color).
+  /// The default primary color as specified by the [design guidelines](https://docs.nextcloud.com/server/latest/developer_manual/design/foundations.html#primary-color).
   static const Color primary = Color(0xFF0082C9);
+
+  /// Nextcloud success.
+  ///
+  /// The success color as specified by the [design guidelines](https://docs.nextcloud.com/server/latest/developer_manual/design/foundations.html#status-and-indicators).
+  static const Color success = Colors.green;
+
+  /// Nextcloud error.
+  ///
+  /// The error color as specified by the [design guidelines](https://docs.nextcloud.com/server/latest/developer_manual/design/foundations.html#status-and-indicators).
+  static const Color error = Colors.red;
+
+  /// Nextcloud warning.
+  ///
+  /// The warning color as specified by the [design guidelines](https://docs.nextcloud.com/server/latest/developer_manual/design/foundations.html#status-and-indicators).
+  static const Color warning = Colors.orange;
 
   /// The [ColorScheme.background] color used on OLED devices.
   ///
