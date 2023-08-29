5c0b2cbc7ec6ba54a3f8ae4e579a06083890a5c1
Nikolas Rimikis <rimikis.nikolas@gmail.com>
2023-08-29 08:33:51 +0200

    test(neon): add some AppStorage and AppImplementation tests
    Signed-off-by: Nikolas Rimikis <rimikis.nikolas@gmail.com>

---
+++ packages/neon/neon/test/app_implementation_test.dart
@@ -0,0 +1,30 @@
+import 'package:mocktail/mocktail.dart';
+import 'package:neon/src/models/app_implementation.dart';
+import 'package:test/test.dart';
+
+// ignore: missing_override_of_must_be_overridden
+class AppImplementationMock extends Mock implements AppImplementation {}
+
+void main() {
+  group('group name', () {
+    test('AccountFind', () {
+      final app1 = AppImplementationMock();
+      final app2 = AppImplementationMock();
+
+      final apps = {
+        app1,
+        app2,
+      };
+
+      when(() => app1.id).thenReturn('app1');
+      when(() => app2.id).thenReturn('app2');
+
+      expect(apps.tryFind(null), isNull);
+      expect(apps.tryFind('invalidID'), isNull);
+      expect(apps.tryFind(app2.id), equals(app2));
+
+      expect(() => apps.find('invalidID'), throwsA(isA<StateError>()));
+      expect(apps.find(app2.id), equals(app2));
+    });
+  });
+}
