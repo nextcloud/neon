febd7b18bec9ce66feec1a1d9a4204bf0edf0b7f
Nikolas Rimikis <leptopoda@users.noreply.github.com>
2023-09-29 22:00:21 +0200

    refactor(neon): remove color property from AccountSettingsTile
    Signed-off-by: Nikolas Rimikis <leptopoda@users.noreply.github.com>

--- packages/neon/neon/lib/src/settings/widgets/account_settings_tile.dart
+++ packages/neon/neon/lib/src/settings/widgets/account_settings_tile.dart
@@ -8,21 +8,19 @@ import 'package:neon/src/widgets/account_tile.dart';
 class AccountSettingsTile extends SettingsTile {
   const AccountSettingsTile({
     required this.account,
-    this.color,
     this.trailing,
     this.onTap,
     super.key,
   });
 
   final Account account;
-  final Color? color;
+
   final Widget? trailing;
   final GestureTapCallback? onTap;
 
   @override
   Widget build(final BuildContext context) => NeonAccountTile(
         account: account,
-        color: color,
         trailing: trailing,
         onTap: onTap,
       );
