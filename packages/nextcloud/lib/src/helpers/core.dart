563bd36f81b43c571be1a2df7e18b07d7b5d56a1
jld3103 <jld3103yt@gmail.com>
2023-09-29 07:04:01 +0200

    refactor(nextcloud): Move version support to helpers
    Signed-off-by: jld3103 <jld3103yt@gmail.com>

--- packages/nextcloud/lib/src/helpers/core.dart
+++ packages/nextcloud/lib/src/helpers/core.dart
@@ -1,5 +1,25 @@
 // ignore_for_file: public_member_api_docs
 
+import 'package:nextcloud/src/api/core.openapi.dart';
+
+/// Version of core/Server supported
+const coreSupportedVersion = 27;
+
+extension CoreVersionSupported on CoreClient {
+  /// Check if the core/Server version is supported by this client
+  ///
+  /// Also returns the supported version number
+  (bool, int) isSupported(final CoreOcsGetCapabilitiesResponseApplicationJson_Ocs_Data capabilities) => (
+        capabilities.version.major == coreSupportedVersion,
+        coreSupportedVersion,
+      );
+}
+
+extension CoreStatusVersionSupported on CoreStatus {
+  /// Check if the core/Server version is supported
+  bool get isSupported => version.startsWith('$coreSupportedVersion.');
+}
+
 enum ShareType {
   user,
   group,
--- packages/nextcloud/lib/src/helpers/news.dart
+++ packages/nextcloud/lib/src/helpers/news.dart
@@ -1,5 +1,23 @@
 // ignore_for_file: public_member_api_docs
 
+import 'package:nextcloud/src/api/news.openapi.dart';
+
+/// API version of the news app supported
+const newsSupportedVersion = 'v1-3';
+
+extension NewsVersionSupported on NewsClient {
+  /// Check if the news app version is supported by this client
+  ///
+  /// Also returns the supported API version number
+  Future<(bool, String)> isSupported() async {
+    final response = await getSupportedApiVersions();
+    return (
+      response.body.apiLevels!.contains(newsSupportedVersion),
+      newsSupportedVersion,
+    );
+  }
+}
+
 /// See https://github.com/nextcloud/news/blob/4a107b3d53c4fe651ac704251b99e04a53cd587f/lib/Db/ListType.php
 enum NewsListType {
   feed,
