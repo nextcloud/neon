85b934e8f99991c488ac0eccc426142938ea8d7f
jld3103 <jld3103yt@gmail.com>
2023-09-30 11:13:48 +0200

    feat(tool,ci): Cache docker images
    Signed-off-by: jld3103 <jld3103yt@gmail.com>

--- tool/dev.sh
+++ tool/dev.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 set -euxo pipefail
 cd "$(dirname "$0")/.."
+source tool/common.sh
 
 ./tool/build-dev-container.sh
 
 echo "Running development instance on http://localhost. To access it in an Android Emulator use http://10.0.2.2"
-docker run --rm -v nextcloud-neon-dev:/usr/src/nextcloud -v nextcloud-neon-dev:/var/www/html -p "80:80" --add-host=host.docker.internal:host-gateway nextcloud-neon-dev
+docker run --rm -v nextcloud-neon-dev:/usr/src/nextcloud -v nextcloud-neon-dev:/var/www/html -p "80:80" --add-host=host.docker.internal:host-gateway "$(image_tag "dev:latest")"
