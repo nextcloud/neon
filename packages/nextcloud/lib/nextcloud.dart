ab4fbc9916fafd9ba9c9b688bfca85688cac3511
jld3103 <jld3103yt@gmail.com>
2023-10-15 14:55:34 +0200

    feat(nextcloud): Export BytesStreamExtension from dynamite_runtime
    Signed-off-by: jld3103 <jld3103yt@gmail.com>

--- packages/nextcloud/lib/nextcloud.dart
+++ packages/nextcloud/lib/nextcloud.dart
@@ -1,6 +1,6 @@
 export 'package:dynamite_runtime/content_string.dart';
 export 'package:dynamite_runtime/http_client.dart'
-    show CookieJar, DynamiteApiException, DynamiteRawResponse, DynamiteResponse;
+    show BytesStreamExtension, CookieJar, DynamiteApiException, DynamiteRawResponse, DynamiteResponse;
 
 export 'ids.dart';
 export 'src/client.dart';
