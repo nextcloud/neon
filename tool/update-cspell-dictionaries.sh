925d8bf4147c74d78c9265a4a5f5286756d0ed5a
jld3103 <jld3103yt@gmail.com>
2023-10-16 21:38:26 +0200

    fix(tool): Fix cspell for Dart raw strings
    Signed-off-by: jld3103 <jld3103yt@gmail.com>

--- tool/update-cspell-dictionaries.sh
+++ tool/update-cspell-dictionaries.sh
@@ -4,5 +4,5 @@ cd "$(dirname "$0")/.."
 
 for file in .cspell/*; do
   rm "$file"
-  cspell lint --quiet --unique --words-only . | tr '[:upper:]' '[:lower:]' | sort -f | uniq > "$file" || true
+  cspell lint --quiet --unique --words-only . | tr '[:upper:]' '[:lower:]' | sed "s/^r'//" | sort -f | uniq > "$file" || true
 done
