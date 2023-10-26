77391e502a73c02e291335e730707a668b8185ee
Nikolas Rimikis <leptopoda@users.noreply.github.com>
2023-10-26 08:19:02 +0200

    docs(neon): document neon settings
    Signed-off-by: Nikolas Rimikis <leptopoda@users.noreply.github.com>

--- packages/neon/neon/lib/src/settings/models/options_category.dart
+++ packages/neon/neon/lib/src/settings/models/options_category.dart
@@ -1,11 +1,15 @@
 import 'package:meta/meta.dart';
 import 'package:neon/src/models/label_builder.dart';
+import 'package:neon/src/settings/models/option.dart';
 
+/// Category of an [Option].
 @immutable
 class OptionsCategory {
+  /// Creates a new Category.
   const OptionsCategory({
     required this.name,
   });
 
+  /// Builder function for the category name.
   final LabelBuilder name;
 }
