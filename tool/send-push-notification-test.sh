9dd756fee695a492de3f3e6fb0b9649f92106649
jld3103 <jld3103yt@gmail.com>
2023-11-01 13:31:13 +0100

    fix(tool): Fix sending push notification test
    Signed-off-by: jld3103 <jld3103yt@gmail.com>

--- tool/send-push-notification-test.sh
+++ tool/send-push-notification-test.sh
@@ -3,4 +3,4 @@ set -euxo pipefail
 cd "$(dirname "$0")/.."
 source tool/common.sh
 
-docker exec -it "$(docker ps | grep "$(image_tag "dev:latest")" | cut -d " " -f 1)" /bin/bash -c "php -f occ notification:test-push $*"
+docker exec -it "$(docker ps | grep "$(image_tag "dev:latest")" | cut -d " " -f 1)" /bin/sh -c "php -f occ notification:test-push $*"
