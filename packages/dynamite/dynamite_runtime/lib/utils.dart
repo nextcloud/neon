4f28acc0b43ac30b78031646015d35e3d752b1d4
Nikolas Rimikis <leptopoda@users.noreply.github.com>
2023-12-23 11:23:27 +0100

    chore(dynamite_runtime): fix last pana complaints for the 0.1.0 release
    Signed-off-by: Nikolas Rimikis <leptopoda@users.noreply.github.com>

--- packages/dynamite/dynamite_runtime/lib/utils.dart
+++ packages/dynamite/dynamite_runtime/lib/utils.dart
@@ -1,3 +1,6 @@
+/// Utility functions used by the generated code.
+library utils;
+
 export 'src/utils/codecs.dart';
 export 'src/utils/some_of_validators.dart';
 export 'src/utils/string_checker.dart';
