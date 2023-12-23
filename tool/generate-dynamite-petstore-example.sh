7b954c80cc329ce3beac6fa13798106e61b0affa
Nikolas Rimikis <leptopoda@users.noreply.github.com>
2023-12-23 12:10:14 +0100

    chore(dynamite): make petstore the example package of dynamite
    Signed-off-by: Nikolas Rimikis <leptopoda@users.noreply.github.com>

--- tool/generate-dynamite-petstore-example.sh
+++ tool/generate-dynamite-petstore-example.sh
@@ -2,10 +2,10 @@
 set -euxo pipefail
 cd "$(dirname "$0")/.."
 
-cp external/openapi-specification/examples/v3.0/petstore-expanded.json packages/dynamite/dynamite_petstore_example/lib/petstore.openapi.json
+cp external/openapi-specification/examples/v3.0/petstore-expanded.json packages/dynamite/dynamite/example/lib/petstore.openapi.json
 
 (
-  cd packages/dynamite/dynamite_petstore_example
+  cd packages/dynamite/dynamite/example
   rm -rf .dart_tool/build/generated/dynamite
   fvm dart pub run build_runner build --delete-conflicting-outputs
   fvm dart fix --apply lib/
