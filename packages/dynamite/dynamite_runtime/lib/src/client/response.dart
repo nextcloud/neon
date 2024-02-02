355229c0d6c3bfa0eb37ffbfc1e28db36c04f5e7
Nikolas Rimikis <leptopoda@users.noreply.github.com>
2024-02-02 18:00:50 +0100

    feat(dynamite_runtime): add ResponseConverter to convert StreamedResponse to DynamiteResponse
    Signed-off-by: Nikolas Rimikis <leptopoda@users.noreply.github.com>

--- packages/dynamite/dynamite_runtime/lib/src/client/response.dart
+++ packages/dynamite/dynamite_runtime/lib/src/client/response.dart
@@ -1,8 +1,8 @@
-import 'dart:async';
+import 'dart:convert';
 import 'dart:typed_data';
 
 import 'package:built_value/serializer.dart';
-import 'package:dynamite_runtime/src/http_extensions.dart';
+import 'package:dynamite_runtime/src/utils/byte_stream_extension.dart';
 import 'package:http/http.dart' as http;
 import 'package:meta/meta.dart';
 
@@ -12,108 +12,74 @@ class DynamiteResponse<B, H> {
   /// Creates a new dynamite response.
   const DynamiteResponse(
     this.statusCode,
-    this._body,
-    this._headers,
+    this.body,
+    this.headers,
   );
+
+  /// Converts the [rawResponse] to a generic dynamite response to free the
+  /// resources taken by the raw parameters.
+  factory DynamiteResponse.fromRawResponse(DynamiteRawResponse<B, H> rawResponse) => DynamiteResponse(
+        rawResponse.statusCode,
+        rawResponse.body,
+        rawResponse.headers,
+      );
 
   /// The status code of the response.
   final int statusCode;
 
-  final B? _body;
-
-  final H? _headers;
-
   /// The decoded body of the response.
-  B get body => _body!;
+  final B body;
 
   /// The decoded headers of the response.
-  H get headers => _headers!;
+  final H headers;
 
   @override
   String toString() => 'DynamiteResponse(data: $body, headers: $headers, statusCode: $statusCode)';
 }
 
-/// Raw response returned by operations of a `DynamiteClient`.
-///
-/// This type itself is serializable.
-///
-/// The api of this type might change without a major bump.
-/// Use methods that return a [DynamiteResponse] instead.
-@experimental
-class DynamiteRawResponse<B, H> {
+/// Raw response that can be efficiently serialized into a json Map.
+class DynamiteRawResponse<B, H> extends DynamiteResponse<B, H> {
   /// Creates a new raw dynamite response.
+  const DynamiteRawResponse({
+    required int statusCode,
+    required B body,
+    required H headers,
+    this.rawHeaders,
+    this.rawBody,
+  }) : super(statusCode, body, headers);
+
+  /// Caches the raw headers for later serialization in [toJson].
+  final Map<String, Object?>? rawHeaders;
+
+  /// Caches the raw response body for later serialization in [toJson].
+  final Object? rawBody;
+
+  /// Serializes this response into json.
   ///
-  /// The [response] will be awaited and deserialized.
-  /// After [future] completes the deserialized response can be accessed
-  /// through [response].
-  DynamiteRawResponse({
-    required Future<http.StreamedResponse> response,
+  /// To revive it again use `DynamiteRawResponse.fromJson` with the same
+  /// serializer and `FullType`s as this.
+  Map<String, Object?> toJson() => {
+        'statusCode': statusCode,
+        'body': rawBody,
+        'headers': rawHeaders,
+      };
+
+  @override
+  String toString() => 'DynamiteResponse(data: $body, headers: $headers, statusCode: $statusCode)';
+}
+
+/// The serializer to convert the raw headers and body from a [http.BaseResponse].
+///
+/// The generics [B] and [H] must match the type returned after deserializing to
+/// [bodyType] and [headersType] respectively.
+@immutable
+class DynamiteSerializer<B, H> {
+  /// Creates a new dynamite serializer.
+  const DynamiteSerializer({
     required this.bodyType,
     required this.headersType,
     required this.serializers,
-  }) {
-    final completer = Completer<DynamiteResponse<B, H>>();
-    future = completer.future;
-
-    // ignore: discarded_futures
-    response.then(
-      (response) async {
-        _rawHeaders = response.headers;
-        final headers = deserializeHeaders<H>(_rawHeaders, serializers, headersType);
-
-        _rawBody = switch (bodyType) {
-          const FullType(Uint8List) => await response.stream.bytes,
-          const FullType(String) => await response.stream.string,
-          _ => await response.stream.json,
-        };
-        final body = deserializeBody<B>(_rawBody, serializers, bodyType);
-
-        _response = DynamiteResponse<B, H>(
-          response.statusCode,
-          body,
-          headers,
-        );
-
-        completer.complete(_response);
-      },
-      onError: completer.completeError,
-    );
-  }
-
-  /// Decodes a raw dynamite response from json data.
-  ///
-  /// The [future] must not be awaited and the  deserialized response can be
-  /// accessed immediately through [response].
-  factory DynamiteRawResponse.fromJson(
-    Map<String, Object?> json, {
-    required Serializers serializers,
-    FullType? bodyType,
-    FullType? headersType,
-  }) {
-    final statusCode = json['statusCode']! as int;
-    final body = deserializeBody<B>(json['body'], serializers, bodyType);
-    final headers = deserializeHeaders<H>(json['headers'], serializers, headersType);
-
-    final response = DynamiteResponse<B, H>(
-      statusCode,
-      body,
-      headers,
-    );
-
-    return DynamiteRawResponse._fromJson(
-      response,
-      bodyType: bodyType,
-      headersType: headersType,
-      serializers: serializers,
-    );
-  }
-
-  DynamiteRawResponse._fromJson(
-    this._response, {
-    required this.bodyType,
-    required this.headersType,
-    required this.serializers,
-  }) : future = Future.value(_response);
+  });
 
   /// The serializers for the header and body.
   final Serializers serializers;
@@ -127,109 +93,86 @@ class DynamiteRawResponse<B, H> {
   ///
   /// This is `null` if the headers type is void.
   final FullType? headersType;
+}
 
-  /// Future of the deserialized response.
-  ///
-  /// After this future completes the response can be accessed synchronously
-  /// through [response].
-  late final Future<DynamiteResponse<B, H>> future;
+/// Converter to transform a [http.StreamedResponse] into a [DynamiteResponse].
+class ResponseConverter<B, H> with Converter<http.StreamedResponse, Future<DynamiteRawResponse<B, H>>> {
+  /// Creates a new response converter
+  const ResponseConverter(this.serializer);
 
-  /// Caches the serialized response body for later serialization in [toJson].
-  ///
-  /// Responses revived with [DynamiteRawResponse.fromJson] are not cached as
-  /// they are not expected to be serialized again.
-  Object? _rawBody;
-
-  Map<String, Object?>? _rawHeaders;
-
-  /// Caches the serialized response headers for later serialization in [toJson].
-  ///
-  /// Responses revived with [DynamiteRawResponse.fromJson] are not cached as
-  /// they are not expected to be serialized again.
-  Map<String, Object?>? get rawHeaders => _rawHeaders;
-
-  DynamiteResponse<B, H>? _response;
-
-  /// Returns the deserialized response synchronously.
-  ///
-  /// Throws a `StateError` if [future] has not completed yet and `this` has
-  /// not been instantiated through [DynamiteRawResponse.fromJson].
-  DynamiteResponse<B, H> get response {
-    final response = _response;
-
-    if (response == null) {
-      throw StateError('The response did not finish yet. Make sure to await `this.future`.');
-    }
-
-    return response;
-  }
-
-  /// Deserializes the body.
-  ///
-  /// Most efficient if the [serialized] value is already the correct type.
-  /// The [bodyType] should represent the return type [B].
-  static B? deserializeBody<B>(Object? serialized, Serializers serializers, FullType? bodyType) {
-    // If we use the more efficient helpers from BytesStreamExtension the serialized value can already be correct.
-    if (serialized is B) {
-      return serialized;
-    }
-
-    if (bodyType != null) {
-      return serializers.deserialize(serialized, specifiedType: bodyType) as B?;
-    }
-
-    return null;
-  }
-
-  /// Serializes the body.
-  Object? serializeBody(B? object) {
-    if (bodyType != null && object != null) {
-      return serializers.serialize(object, specifiedType: bodyType!);
-    }
-
-    return null;
-  }
-
-  /// Deserializes the headers.
-  ///
-  /// Most efficient if the [serialized] value is already the correct type.
-  /// The [headersType] should represent the return type [H].
-  static H? deserializeHeaders<H>(
-    Object? serialized,
-    Serializers serializers,
-    FullType? headersType,
-  ) {
-    // If we use the more efficient helpers from BytesStreamExtension the serialized value can already be correct.
-    if (serialized is H) {
-      return serialized;
-    }
-
-    if (headersType != null) {
-      return serializers.deserialize(serialized, specifiedType: headersType) as H?;
-    }
-
-    return null;
-  }
-
-  /// Serializes the headers.
-  Object? serializeHeaders(H? object) {
-    if (headersType != null && object != null) {
-      return serializers.serialize(object, specifiedType: headersType!);
-    }
-
-    return null;
-  }
-
-  /// Serializes this response into json.
-  ///
-  /// To revive it again use [DynamiteRawResponse.fromJson] with the same
-  /// serializer and `FullType`s as this.
-  Map<String, Object?> toJson() => {
-        'statusCode': response.statusCode,
-        'body': _rawBody ?? serializeBody(response._body),
-        'headers': _rawHeaders ?? serializeHeaders(response._headers),
-      };
+  /// The serializer to convert the raw header and body into [B] and [H].
+  final DynamiteSerializer<B, H> serializer;
 
   @override
-  String toString() => 'DynamiteResponse(${toJson()})';
+  Future<DynamiteRawResponse<B, H>> convert(http.StreamedResponse input) async {
+    final rawHeaders = input.headers;
+    final headers = _deserialize<H>(rawHeaders, serializer.serializers, serializer.headersType);
+
+    final encoding = encodingForHeaders(input.headers);
+    final rawBody = switch (serializer.bodyType) {
+      const FullType(Uint8List) => await input.stream.bytes,
+      const FullType(String) => await input.stream.bytesToString(encoding),
+      _ => await input.stream.bytesToJson(encoding),
+    };
+    final body = _deserialize<B>(rawBody, serializer.serializers, serializer.bodyType);
+
+    return DynamiteRawResponse<B, H>(
+      statusCode: input.statusCode,
+      rawHeaders: rawHeaders,
+      headers: headers as H,
+      rawBody: rawBody,
+      body: body as B,
+    );
+  }
+}
+
+/// Encoder to efficiently convert a raw response to json.
+class RawResponseEncoder<B, H> with Converter<DynamiteRawResponse<B, H>, Map<String, Object?>> {
+  @override
+  Map<String, Object?> convert(DynamiteRawResponse<B, H> input) => {
+        'statusCode': input.statusCode,
+        'body': input.rawBody,
+        'headers': input.rawHeaders,
+      };
+}
+
+/// Encoder to revive the json from a raw response.
+class RawResponseDecoder<B, H> with Converter<Map<String, Object?>, DynamiteResponse<B, H>> {
+  /// Creates a new response converter
+  const RawResponseDecoder(this.serializer);
+
+  /// The serializer to convert the raw header and body into [B] and [H].
+  final DynamiteSerializer<B, H> serializer;
+
+  @override
+  DynamiteResponse<B, H> convert(Map<String, Object?> input) {
+    final rawHeaders = input['headers'];
+    final headers = _deserialize<H>(rawHeaders, serializer.serializers, serializer.headersType);
+
+    final rawBody = input['body'];
+    final body = _deserialize<B>(rawBody, serializer.serializers, serializer.bodyType);
+
+    return DynamiteResponse<B, H>(
+      input['statusCode']! as int,
+      body as B,
+      headers as H,
+    );
+  }
+}
+
+/// Deserializes the given [serialized] data.
+///
+/// Serialization is skipped if the data already represents the desired type.
+/// The [fullType] should represent the return type [T].
+T? _deserialize<T>(Object? serialized, Serializers serializers, FullType? fullType) {
+  // If we use the more efficient helpers from BytesStreamExtension the serialized value can already be correct.
+  if (serialized is T) {
+    return serialized;
+  }
+
+  if (fullType != null) {
+    return serializers.deserialize(serialized, specifiedType: fullType) as T?;
+  }
+
+  return null;
 }
