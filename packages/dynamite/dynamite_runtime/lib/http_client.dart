e525060d852415dec54ca02748bb8cb6ee65ab26
Nikolas Rimikis <leptopoda@users.noreply.github.com>
2024-02-06 20:39:08 +0100

    refactor(dynamite_runtime): split dynamite_client into separate libraries
    Signed-off-by: Nikolas Rimikis <leptopoda@users.noreply.github.com>

--- packages/dynamite/dynamite_runtime/lib/http_client.dart
+++ packages/dynamite/dynamite_runtime/lib/http_client.dart
@@ -1,5 +1,9 @@
 /// The dynamite client to handle http connections.
 library http_client;
 
-export 'src/dynamite_client.dart';
+export 'src/client/authentication.dart';
+export 'src/client/client.dart';
+export 'src/client/exception.dart';
+export 'src/client/request.dart';
+export 'src/client/response.dart';
 export 'src/http_extensions.dart';
