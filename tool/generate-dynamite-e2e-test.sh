4bdfdc7c339730f31f1523d72425fa4e1be7105b
provokateurin <kate@provokateurin.de>
2024-02-27 14:47:34 +0100

    chore(tool): Use `dart run` instead of deprecated `dart pub run`
    Signed-off-by: provokateurin <kate@provokateurin.de>

--- tool/generate-dynamite-e2e-test.sh
+++ tool/generate-dynamite-e2e-test.sh
@@ -5,7 +5,7 @@ cd "$(dirname "$0")/.."
 (
   cd packages/dynamite/dynamite_end_to_end_test
   rm -rf .dart_tool/build/generated/dynamite
-  fvm dart pub run build_runner build --delete-conflicting-outputs
+  fvm dart run build_runner build --delete-conflicting-outputs
   fvm dart fix --apply lib/
   melos run format
 )
--- tool/generate-nextcloud.sh
+++ tool/generate-nextcloud.sh
@@ -6,7 +6,7 @@ cd "$(dirname "$0")/.."
   cd packages/nextcloud
   rm -rf .dart_tool/build/generated/dynamite
   fvm dart run generate_props.dart
-  fvm dart pub run build_runner build --delete-conflicting-outputs
+  fvm dart run build_runner build --delete-conflicting-outputs
   fvm dart run generate_exports.dart
   # For some reason we need to fix and format twice, otherwise not everything gets fixed
   fvm dart fix --apply lib/src/api/
