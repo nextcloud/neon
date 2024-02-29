f9c58c9e01ef4cdeb2cd79532f5482db10941f5d
Nikolas Rimikis <leptopoda@users.noreply.github.com>
2024-02-29 11:26:36 +0100

    feat(dynamite_runtime): add new internal ByteStream extension based on http.ByteStream respects the content-type header when decoding
    Signed-off-by: Nikolas Rimikis <leptopoda@users.noreply.github.com>

--- packages/nextcloud/lib/nextcloud.dart
+++ packages/nextcloud/lib/nextcloud.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: deprecated_member_use
+
 export 'package:dynamite_runtime/http_client.dart'
     show BytesStreamExtension, DynamiteApiException, DynamiteRawResponse, DynamiteResponse, DynamiteStatusCodeException;
 export 'package:dynamite_runtime/models.dart';
