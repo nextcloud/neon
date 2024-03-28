20ede067fd763b1a52e0cffa2c3863ac420e7b7a
Nikolas Rimikis <leptopoda@users.noreply.github.com>
2024-03-28 16:58:06 +0100

    feat(nextcloud): add webdav response converter
    Signed-off-by: Nikolas Rimikis <leptopoda@users.noreply.github.com>

--- packages/nextcloud/lib/src/webdav/utils.dart
+++ packages/nextcloud/lib/src/webdav/utils.dart
@@ -1,6 +1,12 @@
+import 'dart:convert';
+
+import 'package:dynamite_runtime/http_client.dart';
+import 'package:dynamite_runtime/utils.dart';
+import 'package:http/http.dart' as http;
 import 'package:meta/meta.dart';
 import 'package:nextcloud/src/webdav/models.dart';
 import 'package:nextcloud/src/webdav/path_uri.dart';
+import 'package:nextcloud/src/webdav/webdav.dart';
 
 /// Base path used on the server
 final webdavBase = PathUri.parse('/remote.php/webdav');
@@ -24,4 +30,20 @@ Uri constructUri(Uri baseURL, [PathUri? path]) {
     segments.addAll(path.pathSegments);
   }
   return baseURL.replace(pathSegments: segments.where((s) => s.isNotEmpty));
+}
+
+/// Converter to transform a [http.StreamedResponse] into a [DynamiteResponse].
+///
+/// Throws a [DynamiteApiException] on errors.
+final class WebDavResponseConverter with Converter<http.StreamedResponse, Future<WebDavMultistatus>> {
+  /// Creates a new response converter
+  const WebDavResponseConverter();
+
+  @override
+  Future<WebDavMultistatus> convert(http.StreamedResponse input) async {
+    final encoding = encodingForHeaders(input.headers);
+
+    final xml = await input.stream.bytesToXml(encoding);
+    return WebDavMultistatus.fromXmlElement(xml!);
+  }
 }
