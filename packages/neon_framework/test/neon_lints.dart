535b205dba56062f7f9c75051e560f9894690061
provokateurin <kate@provokateurin.de>
2024-03-28 15:31:20 +0100

    feat(neon_lints): Add warnings to avoid dart:html and dart:io
    Signed-off-by: provokateurin <kate@provokateurin.de>

--- packages/neon_framework/test/neon_lints.dart
+++ packages/neon_framework/test/neon_lints.dart
@@ -1,3 +1,10 @@
+// ignore_for_file: unused_import
+
+// expect_lint: avoid_dart_html
+import 'dart:html';
+// expect_lint: avoid_dart_io
+import 'dart:io';
+
 import 'package:flutter/widgets.dart';
 
 // Apparently custom_lint can not be run in the defining package. So lint tests
