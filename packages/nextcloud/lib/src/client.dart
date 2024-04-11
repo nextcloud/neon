9001f1264607ac51a04a1c069772b7bc584761f7
provokateurin <kate@provokateurin.de>
2024-04-11 17:20:09 +0200

    docs(nextcloud): Add documentation to NextcloudClient
    Signed-off-by: provokateurin <kate@provokateurin.de>

--- packages/nextcloud/lib/src/client.dart
+++ packages/nextcloud/lib/src/client.dart
@@ -1,9 +1,21 @@
 import 'package:dynamite_runtime/http_client.dart';
 import 'package:universal_io/io.dart';
 
-// ignore: public_member_api_docs
+/// A client configuring the clients for all Nextcloud APIs.
+///
+/// To access the APIs of a particular app import the extensions through `package:nextcloud/{id}.dart`.
 class NextcloudClient extends DynamiteClient {
-  // ignore: public_member_api_docs
+  /// Creates a new Nextcloud API client.
+  ///
+  /// [baseURL] has to point to the Nextcloud server webroot.
+  ///
+  /// To access authenticated endpoints [loginName] and one of [password] or [appPassword] have to be set.
+  /// Note that not all endpoints can be access by using only the [password], so it is preferred to set the [appPassword] instead.
+  /// [loginName] can be any user identifier like the username or the e-mail.
+  ///
+  /// It is good practice to set the [userAgent] to allow server admins to identify clients.
+  /// A custom HTTP client can be provided through [httpClient].
+  /// Additionally a [cookieJar] can be specified to save cookies across requests.
   NextcloudClient(
     super.baseURL, {
     String? loginName,
