ad608fec0e232616db77d924b2ad90ea26458fae
Nikolas Rimikis <leptopoda@users.noreply.github.com>
2024-04-27 11:49:52 +0200

    refactor(dynamite_runtime)!: remove deprecated methods.
    Signed-off-by: Nikolas Rimikis <leptopoda@users.noreply.github.com>

--- packages/dynamite/dynamite_runtime/lib/utils.dart
+++ packages/dynamite/dynamite_runtime/lib/utils.dart
@@ -4,16 +4,8 @@
 /// the client itself.
 library utils;
 
-// ignore: deprecated_member_use_from_same_package
-@Deprecated('Use checkers from json_schema_validation.dart')
-export 'src/utils/array_checker.dart';
-
 export 'src/utils/byte_stream_extension.dart';
 export 'src/utils/codecs.dart';
 export 'src/utils/json_schema_validation.dart';
 export 'src/utils/some_of_validators.dart';
-
-// ignore: deprecated_member_use_from_same_package
-@Deprecated('Use checkers from json_schema_validation.dart')
-export 'src/utils/string_checker.dart';
 export 'src/utils/uri.dart';
