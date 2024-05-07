c1cc5e11103ca4fa3a7eae72ed4d31b362b18db2
provokateurin <kate@provokateurin.de>
2024-05-07 21:44:55 +0200

    fix(neon_news): Move dialog utils to src/
    Signed-off-by: provokateurin <kate@provokateurin.de>

--- packages/neon/neon_news/lib/src/widgets/folder_floating_action_button.dart
+++ packages/neon/neon_news/lib/src/widgets/folder_floating_action_button.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:neon_framework/theme.dart';
 import 'package:neon_news/l10n/localizations.dart';
 import 'package:neon_news/src/blocs/news.dart';
-import 'package:neon_news/utils/dialog.dart';
+import 'package:neon_news/src/utils/dialog.dart';
 
 class NewsFolderFloatingActionButton extends StatelessWidget {
   const NewsFolderFloatingActionButton({
