210d84e6cad5240f8a149ff2d99bbdb6a9d7aee5
provokateurin <kate@provokateurin.de>
2024-05-23 14:58:49 +0200

    fix(neon_talk): Ensure constraints for rich object file previews are set correctly
    Signed-off-by: provokateurin <kate@provokateurin.de>

--- packages/neon/neon_talk/lib/src/widgets/rich_object/file_preview.dart
+++ packages/neon/neon_talk/lib/src/widgets/rich_object/file_preview.dart
@@ -18,66 +18,45 @@ class TalkRichObjectFilePreview extends StatelessWidget {
   Widget build(BuildContext context) {
     final maxHeight = MediaQuery.sizeOf(context).height / 2;
 
-    var width = _parseDimension(parameter.width);
-    var height = _parseDimension(parameter.height);
-    if (width != null && height != null) {
+    Size? logicalSize;
+    var deviceSize = const Size(-1, -1);
+
+    if (parameter.width != null && parameter.height != null) {
       final devicePixelRatio = MediaQuery.of(context).devicePixelRatio;
 
-      var size = Size(width.toDouble(), height.toDouble());
+      deviceSize = Size(_parseDimension(parameter.width!).toDouble(), _parseDimension(parameter.height!).toDouble());
 
       // Convert to logical pixels
-      size /= devicePixelRatio;
+      logicalSize = deviceSize / devicePixelRatio;
 
       // Constrain size to max height but keep aspect ratio
-      if (size.height > maxHeight) {
-        size = size * (maxHeight / size.height);
+      if (logicalSize.height > maxHeight) {
+        logicalSize = logicalSize * (maxHeight / logicalSize.height);
       }
 
       // Convert back to device pixels
-      size *= devicePixelRatio;
-
-      width = size.width.toInt();
-      height = size.height.toInt();
+      deviceSize = logicalSize * devicePixelRatio;
     }
 
-    height ??= -1;
-    width ??= -1;
-
     return ConstrainedBox(
-      constraints: BoxConstraints(
-        maxHeight: maxHeight,
-      ),
+      constraints: logicalSize != null
+          ? BoxConstraints.tight(logicalSize)
+          : BoxConstraints.loose(Size(double.infinity, maxHeight)),
       child: Tooltip(
         message: parameter.name,
         child: NeonApiImage(
-          cacheKey: 'preview-${parameter.path!}-$width-$height',
+          cacheKey: 'preview-${parameter.path!}-${deviceSize.width.toInt()}-${deviceSize.height.toInt()}',
           etag: parameter.etag,
           expires: null,
           getRequest: (client) => client.core.preview.$getPreviewByFileId_Request(
             fileId: int.parse(parameter.id),
-            x: width,
-            y: height,
+            x: deviceSize.width.toInt(),
+            y: deviceSize.height.toInt(),
           ),
         ),
       ),
     );
   }
 
-  int? _parseDimension(({int? $int, String? string})? dimension) {
-    if (dimension == null) {
-      return null;
-    }
-
-    final $int = dimension.$int;
-    if ($int != null) {
-      return $int;
-    }
-
-    final string = dimension.string;
-    if (string != null) {
-      return int.parse(string);
-    }
-
-    throw ArgumentError(r'One of $int or string has to be not-null');
-  }
+  int _parseDimension(({int? $int, String? string}) dimension) => dimension.$int ?? int.parse(dimension.string!);
 }
