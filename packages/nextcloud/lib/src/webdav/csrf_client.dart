2c8e567319d5cdbe20e7df2bafdcdb8ef699b8fd
provokateurin <kate@provokateurin.de>
2024-06-29 18:30:16 +0200

    fix(nextcloud): Clear WebDAV CSRF token after authorization error
    Signed-off-by: provokateurin <kate@provokateurin.de>

--- packages/nextcloud/lib/src/webdav/csrf_client.dart
+++ packages/nextcloud/lib/src/webdav/csrf_client.dart
@@ -1,5 +1,6 @@
 import 'package:dynamite_runtime/http_client.dart';
 import 'package:http/http.dart' as http;
+import 'package:logging/logging.dart';
 import 'package:nextcloud/nextcloud.dart';
 
 /// A [http.Client] that sends the Nextcloud CSRF token.
@@ -15,12 +16,16 @@ final class WebDavCSRFClient with http.BaseClient {
 
   final NextcloudClient _inner;
 
+  final _log = Logger('WebDavCSRFClient');
+
   /// The request token sent by the [WebDavCSRFClient].
   String? _token;
 
   @override
   Future<http.StreamedResponse> send(http.BaseRequest request) async {
     if (_token == null) {
+      _log.fine('Acquiring new CSRF token for WebDAV');
+
       final streamedResponse = await _inner.send(http.Request('GET', Uri.parse('${_inner.baseURL}/index.php')));
       final response = await http.Response.fromStream(streamedResponse);
       if (streamedResponse.statusCode >= 300) {
@@ -38,6 +43,12 @@ final class WebDavCSRFClient with http.BaseClient {
     final streamedResponse = await _inner.send(request);
 
     if (streamedResponse.statusCode >= 300) {
+      if (streamedResponse.statusCode == 401) {
+        // Clear the token just in case it expired and lead to the failure.
+        _log.fine('Clearing CSRF token for WebDAV');
+        _token = null;
+      }
+
       final response = await http.Response.fromStream(streamedResponse);
 
       throw DynamiteStatusCodeException(response);
