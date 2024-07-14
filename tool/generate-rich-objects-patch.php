82c5418c4ff0db8bef5cd0b31d726fcb827e840b
provokateurin <kate@provokateurin.de>
2024-07-14 20:37:56 +0200

    chore: wip

--- tool/generate-rich-objects-patch.php
+++ tool/generate-rich-objects-patch.php
@@ -3,39 +3,66 @@
 
 declare(strict_types=1);
 
+$minVersion = '28.0.0';
+
 include('external/nextcloud-server/lib/public/RichObjectStrings/Definitions.php');
 $definitions = (new OCP\RichObjectStrings\Definitions())->definitions;
 
-$properties = [
-    'type' => [
-        'type' => 'string',
-        'enum' => array_keys($definitions),
+$schemas = [];
+$discriminatorSchema = [
+    'oneOf' => [],
+    'discriminator' => [
+        'propertyName' => 'type',
+        'mapping' => [],
     ],
-    'id' => ['type' => 'string'],
-    'name' => ['type' => 'string'],
 ];
 
 foreach ($definitions as $type => $object) {
+    $schema = [
+        'type' => 'object',
+    ];
+
+    $required = ['type'];
+    $properties = [
+        'type' => [
+            'type' => 'string',
+        ],
+    ];
+
     foreach ($object['parameters'] as $name => $parameter) {
-        $properties[$name] ??= [
+        if ($parameter['required'] === true && version_compare($parameter['since'], $minVersion, '<=')) {
+            $required[] = $name;
+        }
+
+        $properties[$name] = [
             'type' => 'string',
             'description' => $parameter['description'],
             'example' => $parameter['example'],
         ];
     }
+
+    $schema['required'] = $required;
+    $schema['properties'] = $properties;
+
+    $schemaName = 'RichObjectParameter' . ucfirst(str_replace('-', '', ucwords($type, '-')));
+    $schemas[$schemaName] = $schema;
+
+    $discriminatorSchema['oneOf'][] = [
+        '$ref' => '#/components/schemas/' . $schemaName,
+    ];
+    $discriminatorSchema['discriminator']['mapping'][$type] = '#/components/schemas/' . $schemaName;
 }
 
-$patch = [
-    [
+$schemas['RichObjectParameter'] = $discriminatorSchema;
+
+$patch = [];
+foreach ($schemas as $name => $schema) {
+    $patch[] = [
         'op' => 'add',
-        'path' => '/components/schemas/RichObjectParameter',
-        'value' => [
-            'type' => 'object',
-            'required' => ['type', 'id', 'name'],
-            'properties' => $properties,
-        ],
-    ],
-];
+        'path' => '/components/schemas/' . $name,
+        'value' => $schema,
+    ];
+}
 
 $data = json_encode($patch, JSON_THROW_ON_ERROR | JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT);
 if ($data === false) {
