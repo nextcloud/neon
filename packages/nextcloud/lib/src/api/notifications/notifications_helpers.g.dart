c4be1b2328c1e788dfb162e4a0db3eec69255306
provokateurin <kate@provokateurin.de>
2024-10-28 20:22:14 +0100

    fix(nextcloud): Add missing nids and delete-multiple fields to DecryptedSubject
    Signed-off-by: provokateurin <kate@provokateurin.de>

--- packages/nextcloud/lib/src/api/notifications/notifications_helpers.g.dart
+++ packages/nextcloud/lib/src/api/notifications/notifications_helpers.g.dart
@@ -25,6 +25,12 @@ class _$DecryptedSubjectSerializer implements StructuredSerializer<DecryptedSubj
         ..add('nid')
         ..add(serializers.serialize(value, specifiedType: const FullType(int)));
     }
+    value = object.nids;
+    if (value != null) {
+      result
+        ..add('nids')
+        ..add(serializers.serialize(value, specifiedType: const FullType(BuiltList, [FullType(int)])));
+    }
     value = object.app;
     if (value != null) {
       result
@@ -55,6 +61,12 @@ class _$DecryptedSubjectSerializer implements StructuredSerializer<DecryptedSubj
         ..add('delete')
         ..add(serializers.serialize(value, specifiedType: const FullType(bool)));
     }
+    value = object.deleteMultiple;
+    if (value != null) {
+      result
+        ..add('delete-multiple')
+        ..add(serializers.serialize(value, specifiedType: const FullType(bool)));
+    }
     value = object.deleteAll;
     if (value != null) {
       result
@@ -78,6 +90,10 @@ class _$DecryptedSubjectSerializer implements StructuredSerializer<DecryptedSubj
         case 'nid':
           result.nid = serializers.deserialize(value, specifiedType: const FullType(int)) as int?;
           break;
+        case 'nids':
+          result.nids.replace(serializers.deserialize(value, specifiedType: const FullType(BuiltList, [FullType(int)]))!
+              as BuiltList<Object?>);
+          break;
         case 'app':
           result.app = serializers.deserialize(value, specifiedType: const FullType(String)) as String?;
           break;
@@ -93,6 +109,9 @@ class _$DecryptedSubjectSerializer implements StructuredSerializer<DecryptedSubj
         case 'delete':
           result.delete = serializers.deserialize(value, specifiedType: const FullType(bool)) as bool?;
           break;
+        case 'delete-multiple':
+          result.deleteMultiple = serializers.deserialize(value, specifiedType: const FullType(bool)) as bool?;
+          break;
         case 'delete-all':
           result.deleteAll = serializers.deserialize(value, specifiedType: const FullType(bool)) as bool?;
           break;
@@ -107,6 +126,8 @@ class _$DecryptedSubject extends DecryptedSubject {
   @override
   final int? nid;
   @override
+  final BuiltList<int>? nids;
+  @override
   final String? app;
   @override
   final String? subject;
@@ -117,12 +138,24 @@ class _$DecryptedSubject extends DecryptedSubject {
   @override
   final bool? delete;
   @override
+  final bool? deleteMultiple;
+  @override
   final bool? deleteAll;
 
   factory _$DecryptedSubject([void Function(DecryptedSubjectBuilder)? updates]) =>
       (DecryptedSubjectBuilder()..update(updates))._build();
 
-  _$DecryptedSubject._({this.nid, this.app, this.subject, this.type, this.id, this.delete, this.deleteAll}) : super._();
+  _$DecryptedSubject._(
+      {this.nid,
+      this.nids,
+      this.app,
+      this.subject,
+      this.type,
+      this.id,
+      this.delete,
+      this.deleteMultiple,
+      this.deleteAll})
+      : super._();
 
   @override
   DecryptedSubject rebuild(void Function(DecryptedSubjectBuilder) updates) => (toBuilder()..update(updates)).build();
@@ -135,11 +168,13 @@ class _$DecryptedSubject extends DecryptedSubject {
     if (identical(other, this)) return true;
     return other is DecryptedSubject &&
         nid == other.nid &&
+        nids == other.nids &&
         app == other.app &&
         subject == other.subject &&
         type == other.type &&
         id == other.id &&
         delete == other.delete &&
+        deleteMultiple == other.deleteMultiple &&
         deleteAll == other.deleteAll;
   }
 
@@ -147,11 +182,13 @@ class _$DecryptedSubject extends DecryptedSubject {
   int get hashCode {
     var _$hash = 0;
     _$hash = $jc(_$hash, nid.hashCode);
+    _$hash = $jc(_$hash, nids.hashCode);
     _$hash = $jc(_$hash, app.hashCode);
     _$hash = $jc(_$hash, subject.hashCode);
     _$hash = $jc(_$hash, type.hashCode);
     _$hash = $jc(_$hash, id.hashCode);
     _$hash = $jc(_$hash, delete.hashCode);
+    _$hash = $jc(_$hash, deleteMultiple.hashCode);
     _$hash = $jc(_$hash, deleteAll.hashCode);
     _$hash = $jf(_$hash);
     return _$hash;
@@ -161,11 +198,13 @@ class _$DecryptedSubject extends DecryptedSubject {
   String toString() {
     return (newBuiltValueToStringHelper(r'DecryptedSubject')
           ..add('nid', nid)
+          ..add('nids', nids)
           ..add('app', app)
           ..add('subject', subject)
           ..add('type', type)
           ..add('id', id)
           ..add('delete', delete)
+          ..add('deleteMultiple', deleteMultiple)
           ..add('deleteAll', deleteAll))
         .toString();
   }
@@ -177,6 +216,10 @@ class DecryptedSubjectBuilder implements Builder<DecryptedSubject, DecryptedSubj
   int? _nid;
   int? get nid => _$this._nid;
   set nid(int? nid) => _$this._nid = nid;
+
+  ListBuilder<int>? _nids;
+  ListBuilder<int> get nids => _$this._nids ??= ListBuilder<int>();
+  set nids(ListBuilder<int>? nids) => _$this._nids = nids;
 
   String? _app;
   String? get app => _$this._app;
@@ -198,6 +241,10 @@ class DecryptedSubjectBuilder implements Builder<DecryptedSubject, DecryptedSubj
   bool? get delete => _$this._delete;
   set delete(bool? delete) => _$this._delete = delete;
 
+  bool? _deleteMultiple;
+  bool? get deleteMultiple => _$this._deleteMultiple;
+  set deleteMultiple(bool? deleteMultiple) => _$this._deleteMultiple = deleteMultiple;
+
   bool? _deleteAll;
   bool? get deleteAll => _$this._deleteAll;
   set deleteAll(bool? deleteAll) => _$this._deleteAll = deleteAll;
@@ -208,11 +255,13 @@ class DecryptedSubjectBuilder implements Builder<DecryptedSubject, DecryptedSubj
     final $v = _$v;
     if ($v != null) {
       _nid = $v.nid;
+      _nids = $v.nids?.toBuilder();
       _app = $v.app;
       _subject = $v.subject;
       _type = $v.type;
       _id = $v.id;
       _delete = $v.delete;
+      _deleteMultiple = $v.deleteMultiple;
       _deleteAll = $v.deleteAll;
       _$v = null;
     }
@@ -234,9 +283,29 @@ class DecryptedSubjectBuilder implements Builder<DecryptedSubject, DecryptedSubj
   DecryptedSubject build() => _build();
 
   _$DecryptedSubject _build() {
-    final _$result = _$v ??
-        _$DecryptedSubject._(
-            nid: nid, app: app, subject: subject, type: type, id: id, delete: delete, deleteAll: deleteAll);
+    _$DecryptedSubject _$result;
+    try {
+      _$result = _$v ??
+          _$DecryptedSubject._(
+              nid: nid,
+              nids: _nids?.build(),
+              app: app,
+              subject: subject,
+              type: type,
+              id: id,
+              delete: delete,
+              deleteMultiple: deleteMultiple,
+              deleteAll: deleteAll);
+    } catch (_) {
+      late String _$failedField;
+      try {
+        _$failedField = 'nids';
+        _nids?.build();
+      } catch (e) {
+        throw BuiltValueNestedFieldError(r'DecryptedSubject', _$failedField, e.toString());
+      }
+      rethrow;
+    }
     replace(_$result);
     return _$result;
   }
