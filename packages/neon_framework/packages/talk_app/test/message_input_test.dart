055a2e4b2be2e60a93675943885f521bc232889e
provokateurin <kate@provokateurin.de>
2024-11-02 19:28:21 +0100

    feat(neon_rich_text): Render markdown
    Signed-off-by: provokateurin <kate@provokateurin.de>

--- packages/neon_framework/packages/talk_app/test/message_input_test.dart
+++ packages/neon_framework/packages/talk_app/test/message_input_test.dart
@@ -279,6 +279,7 @@ void main() {
     when(() => chatMessage.actorDisplayName).thenReturn('test');
     when(() => chatMessage.message).thenReturn('message');
     when(() => chatMessage.messageParameters).thenReturn(BuiltMap());
+    when(() => chatMessage.markdown).thenReturn(false);
 
     replyTo.add(chatMessage);
     await tester.pumpAndSettle();
@@ -315,6 +316,7 @@ void main() {
     when(() => chatMessage.actorDisplayName).thenReturn('test');
     when(() => chatMessage.message).thenReturn('message');
     when(() => chatMessage.messageParameters).thenReturn(BuiltMap());
+    when(() => chatMessage.markdown).thenReturn(false);
 
     editing.add(chatMessage);
     await tester.pumpAndSettle();
