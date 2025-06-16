afd0f2e94716b9ae71358abb13755862cac88662
provokateurin <kate@provokateurin.de>
2025-06-16 07:19:29 +0200

    fix(neon_framework): Regenerate go_router routes after go_router_builder ^3 update
    Signed-off-by: provokateurin <kate@provokateurin.de>

--- packages/neon_framework/lib/src/router.g.dart
+++ packages/neon_framework/lib/src/router.g.dart
@@ -14,63 +14,75 @@ List<RouteBase> get $appRoutes => [
 RouteBase get $homeRoute => GoRouteData.$route(
       path: '/',
       name: 'home',
-      factory: $HomeRouteExtension._fromState,
+      factory: _$HomeRoute._fromState,
       routes: [
         GoRouteData.$route(
           path: 'settings',
           name: 'Settings',
-          factory: $SettingsRouteExtension._fromState,
+          factory: _$SettingsRoute._fromState,
           routes: [
             GoRouteData.$route(
               path: 'apps/:appid',
               name: 'AppImplementationSettings',
-              factory: $AppImplementationSettingsRouteExtension._fromState,
+              factory: _$AppImplementationSettingsRoute._fromState,
             ),
             GoRouteData.$route(
               path: 'account/:accountID',
               name: 'AccountSettings',
-              factory: $AccountSettingsRouteExtension._fromState,
+              factory: _$AccountSettingsRoute._fromState,
             ),
           ],
         ),
       ],
     );
 
-extension $HomeRouteExtension on HomeRoute {
+mixin _$HomeRoute on GoRouteData {
   static HomeRoute _fromState(GoRouterState state) => const HomeRoute();
 
+  @override
   String get location => GoRouteData.$location(
         '/',
       );
 
+  @override
   void go(BuildContext context) => context.go(location);
 
+  @override
   Future<T?> push<T>(BuildContext context) => context.push<T>(location);
 
+  @override
   void pushReplacement(BuildContext context) => context.pushReplacement(location);
 
+  @override
   void replace(BuildContext context) => context.replace(location);
 }
 
-extension $SettingsRouteExtension on SettingsRoute {
+mixin _$SettingsRoute on GoRouteData {
   static SettingsRoute _fromState(GoRouterState state) => SettingsRoute(
         initialCategory:
             _$convertMapValue('initial-category', state.uri.queryParameters, _$SettingsCategoriesEnumMap._$fromName),
       );
 
+  SettingsRoute get _self => this as SettingsRoute;
+
+  @override
   String get location => GoRouteData.$location(
         '/settings',
         queryParams: {
-          if (initialCategory != null) 'initial-category': _$SettingsCategoriesEnumMap[initialCategory!],
+          if (_self.initialCategory != null) 'initial-category': _$SettingsCategoriesEnumMap[_self.initialCategory!],
         },
       );
 
+  @override
   void go(BuildContext context) => context.go(location);
 
+  @override
   Future<T?> push<T>(BuildContext context) => context.push<T>(location);
 
+  @override
   void pushReplacement(BuildContext context) => context.pushReplacement(location);
 
+  @override
   void replace(BuildContext context) => context.replace(location);
 }
 
@@ -84,80 +96,101 @@ const _$SettingsCategoriesEnumMap = {
   SettingsCategories.other: 'other',
 };
 
-extension $AppImplementationSettingsRouteExtension on AppImplementationSettingsRoute {
+mixin _$AppImplementationSettingsRoute on GoRouteData {
   static AppImplementationSettingsRoute _fromState(GoRouterState state) => AppImplementationSettingsRoute(
         appid: state.pathParameters['appid']!,
       );
 
+  AppImplementationSettingsRoute get _self => this as AppImplementationSettingsRoute;
+
+  @override
   String get location => GoRouteData.$location(
-        '/settings/apps/${Uri.encodeComponent(appid)}',
+        '/settings/apps/${Uri.encodeComponent(_self.appid)}',
       );
 
+  @override
   void go(BuildContext context) => context.go(location);
 
+  @override
   Future<T?> push<T>(BuildContext context) => context.push<T>(location);
 
+  @override
   void pushReplacement(BuildContext context) => context.pushReplacement(location);
 
+  @override
   void replace(BuildContext context) => context.replace(location);
 }
 
-extension $AccountSettingsRouteExtension on AccountSettingsRoute {
+mixin _$AccountSettingsRoute on GoRouteData {
   static AccountSettingsRoute _fromState(GoRouterState state) => AccountSettingsRoute(
         accountID: state.pathParameters['accountID']!,
       );
 
+  AccountSettingsRoute get _self => this as AccountSettingsRoute;
+
+  @override
   String get location => GoRouteData.$location(
-        '/settings/account/${Uri.encodeComponent(accountID)}',
+        '/settings/account/${Uri.encodeComponent(_self.accountID)}',
       );
 
+  @override
   void go(BuildContext context) => context.go(location);
 
+  @override
   Future<T?> push<T>(BuildContext context) => context.push<T>(location);
 
+  @override
   void pushReplacement(BuildContext context) => context.pushReplacement(location);
 
+  @override
   void replace(BuildContext context) => context.replace(location);
 }
 
 T? _$convertMapValue<T>(
   String key,
   Map<String, String> map,
-  T Function(String) converter,
+  T? Function(String) converter,
 ) {
   final value = map[key];
   return value == null ? null : converter(value);
 }
 
 extension<T extends Enum> on Map<T, String> {
-  T _$fromName(String value) => entries.singleWhere((element) => element.value == value).key;
+  T? _$fromName(String? value) => entries.where((element) => element.value == value).firstOrNull?.key;
 }
 
 RouteBase get $loginRoute => GoRouteData.$route(
       path: '/login',
       name: 'login',
-      factory: $LoginRouteExtension._fromState,
+      factory: _$LoginRoute._fromState,
     );
 
-extension $LoginRouteExtension on LoginRoute {
+mixin _$LoginRoute on GoRouteData {
   static LoginRoute _fromState(GoRouterState state) => LoginRoute(
-        serverUrl: _$convertMapValue('server-url', state.uri.queryParameters, Uri.parse),
+        serverUrl: _$convertMapValue('server-url', state.uri.queryParameters, Uri.tryParse),
         qrCode: state.uri.queryParameters['qr-code'],
       );
 
+  LoginRoute get _self => this as LoginRoute;
+
+  @override
   String get location => GoRouteData.$location(
         '/login',
         queryParams: {
-          if (serverUrl != null) 'server-url': serverUrl!.toString(),
-          if (qrCode != null) 'qr-code': qrCode,
+          if (_self.serverUrl != null) 'server-url': _self.serverUrl!.toString(),
+          if (_self.qrCode != null) 'qr-code': _self.qrCode,
         },
       );
 
+  @override
   void go(BuildContext context) => context.go(location);
 
+  @override
   Future<T?> push<T>(BuildContext context) => context.push<T>(location);
 
+  @override
   void pushReplacement(BuildContext context) => context.pushReplacement(location);
 
+  @override
   void replace(BuildContext context) => context.replace(location);
 }
