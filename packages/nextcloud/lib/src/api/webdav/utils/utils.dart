ee050b0904a2c930101546a72c33fccb95816a69
provokateurin <kate@provokateurin.de>
2026-03-27 22:01:45 +0100

    chore: Remove nextcloud package
    Signed-off-by: provokateurin <kate@provokateurin.de>

--- packages/nextcloud/lib/src/api/webdav/utils/utils.dart
+++
@@ -1,5 +0,0 @@
-export 'options_parser.dart';
-export 'webdav_response_converter.dart';
-export 'webdav_uri.dart';
-export 'xml_date_time_converter.dart';
-export 'xml_duration_converter.dart';
--- packages/nextcloud/packages/nextcloud_test_presets/bin/generate_presets.dart
+++
@@ -1,5 +0,0 @@
-import 'package:nextcloud_test_presets/presets_builder.dart';
-
-Future<void> main() async {
-  await generatePresets();
-}
