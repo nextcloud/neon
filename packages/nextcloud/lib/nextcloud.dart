8433a5fa1388f0845c7671a2f052cfe2d960d6ee
provokateurin <kate@provokateurin.de>
2026-03-27 22:01:45 +0100

    chore: Remove nextcloud package
    Signed-off-by: provokateurin <kate@provokateurin.de>

--- packages/nextcloud/lib/nextcloud.dart
+++
@@ -1,6 +0,0 @@
-export 'package:dynamite_runtime/http_client.dart'
-    show DynamiteApiException, DynamiteResponse, DynamiteStatusCodeException;
-export 'package:dynamite_runtime/models.dart';
-
-export 'src/models/models.dart';
-export 'src/nextcloud_client.dart';
